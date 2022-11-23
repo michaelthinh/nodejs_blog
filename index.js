de109ac3bd52435e10ee04c5892bb50055198dd1
MichaelThinh <20120196@student.hcmus.edu.vn>
2022-11-23 13:56:18 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// Route
+
+// function(req,res){res.send("Hello World!")};
+
+app.get("/tin-tuc", (req, res) => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  res.send("Hello World!");
+});
+
+// 127.0.0.1 : localhost
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
