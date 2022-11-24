3679a31b8ee62f896f5f3ca38dea903734259385
MichaelThinh <20120196@student.hcmus.edu.vn>
2022-11-24 12:40:38 +0700

    POST method

--- src/index.js
+++ src/index.js
@@ -8,8 +8,16 @@ const port = 3000;
 // Đọc ảnh
 app.use(express.static(path.join(__dirname, "public")));
 
+// Add middleware
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine(
@@ -32,6 +40,16 @@ app.get("/", (req, res) => {
 // news path
 app.get("/news", (req, res) => {
   res.render("news");
+});
+
+// search path
+app.get("/search", (req, res) => {
+  res.render("search");
+});
+
+app.post("/search", (req, res) => {
+  console.log(req.body);
+  res.send("");
 });
 
 // 127.0.0.1 : localhost
